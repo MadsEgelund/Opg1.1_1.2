1f2606b63d2a42292c772199a1f28d36cbab54c1
MadsEgelund <mads324s@edu.easj.dk>
2017-09-14 15:17:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Opgave 1.1
+        System.out.println("Welcome to Java");
+        System.out.println("Learning JAva Now");
+        System.out.println("Programming is fun");
+
+
+        //Opgave 1.2
+        int etTal = 5;
+        String enTekst = "I love Java";
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println(enTekst);
+        }
+    }
+}
